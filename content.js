cc88018eb53c3fc09d5675428d93b2e3401c49b2
Hisham <hhuneidie@gmail.com>
2023-04-06 09:57:55 +0200

    Add key events to change the speed

--- content.js
+++ content.js
@@ -1,8 +1,64 @@
+// Info:
+//     document = DOM de la web
+//     chrome = SI definido
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "speed-up") {
-        const videos = document.getElementsByTagName("video");
-        for (const video of videos) {
-            video.playbackRate = Number(message.speed);
+        speedUp(message.speed);
+    }
+});
+
+let intervals = {};
+let command = "";
+document.addEventListener("keypress", e => {
+    if ( e.shiftKey ) {
+        if ( !intervals.command ) {
+            intervals.command = setTimeout(() => {
+                execCommand();
+            }, 1000);
+        }
+        switch (e.code) {
+            case 'Digit0':
+            case 'Digit1':
+            case 'Digit2':
+            case 'Digit5':
+            case 'Digit8':
+                command += e.code.substring(5);
+                break;
         }
     }
 });
+
+function execCommand() {
+    switch (command) {
+        case "5":
+            speedUp("0.50");
+            break;
+        case "1":
+            speedUp("1.00");
+            break;
+        case "12":
+            speedUp("1.25");
+            break;
+        case "15":
+            speedUp("1.50");
+            break;
+        case "18":
+            speedUp("1.75");
+            break;
+        case "2":
+            speedUp("2.00");
+            break;
+    }
+    clearTimeout(intervals.command);
+    delete intervals.command;
+    command = "";
+}
+
+
+function speedUp(speed) {
+    const videos = document.getElementsByTagName("video");
+    for (const video of videos) {
+        video.playbackRate = Number(speed);
+    }
+}
